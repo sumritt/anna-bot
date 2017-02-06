55772d1797258548e08112608977ec671fc08108
dkovacevic <dejankov@gmail.com>
2017-02-06 15:09:44 +0100

    Initial commit

---
+++ src/main/java/com/wire/bots/anna/AnnaConfig.java
@@ -0,0 +1,64 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+package com.wire.bots.anna;
+
+import javax.validation.constraints.NotNull;
+
+public class AnnaConfig extends com.wire.bots.sdk.Configuration {
+    public String name;
+    public int accent;
+
+    @NotNull
+    public PandoraConfig pandora;
+    public long delay = 2; // delay in mills when replying
+
+    public PandoraConfig getPandora() {
+        return pandora;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAccent() {
+        return accent;
+    }
+
+    public long getDelay() {
+        return delay;
+    }
+
+    class PandoraConfig {
+        String appId;
+        String userKey;
+        String bot;
+
+        public String getAppId() {
+            return appId;
+        }
+
+        public String getUserKey() {
+            return userKey;
+        }
+
+        public String getBot() {
+            return bot;
+        }
+    }
+}
---
+++ src/main/java/com/wire/bots/anna/Pandora.java
@@ -0,0 +1,62 @@
+package com.wire.bots.anna;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.wire.bots.sdk.Logger;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Pandora {
+    public static String PANDORA_URL = "https://aiaas.pandorabots.com";
+
+    private final Client client;
+    private final String appId;
+    private final String userKey;
+    private final String pandoraBot;
+
+    public Pandora(AnnaConfig config) {
+        this.appId = config.getPandora().getAppId();
+        this.userKey = config.getPandora().getUserKey();
+        this.pandoraBot = config.getPandora().getBot();
+
+        ClientConfig cfg = new ClientConfig(JacksonJsonProvider.class);
+        cfg.property(ClientProperties.SUPPRESS_HTTP_COMPLIANCE_VALIDATION, true);
+        client = JerseyClientBuilder.createClient(cfg);
+    }
+
+    public ArrayList<String> talk(String clientName, String text) throws IOException {
+        Response response = client.target(PANDORA_URL)
+                .path("talk")
+                .path(appId)
+                .path(pandoraBot)
+                .queryParam("user_key", userKey)
+                .queryParam("client_name", clientName)
+                .queryParam("input", text)
+                .queryParam("recent", true)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(null));
+
+        if (response.getStatus() > 300) {
+            String msg = response.readEntity(String.class);
+            Logger.warning(msg);
+            throw new IOException(msg);
+        }
+
+        return response.readEntity(PandoraResponse.class).responses;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class PandoraResponse {
+        public ArrayList<String> responses;
+    }
+}
